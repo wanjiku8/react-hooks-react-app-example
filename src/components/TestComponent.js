39b679b2ae4fce6afac774effaf243f92a0b49ee
Faith <Faith@acesolution.africa>
2025-01-21 18:15:01 +0300

    commit

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,20 +1,8 @@
+// TestComponent.js
 import React from "react";
 
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
 function TestComponent() {
-  return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
-    </div>
-  );
+  return <p>This is a test component!</p>;
 }
 
 export default TestComponent;
